4cdc5973e273c1c6e31d49b58bed4b8c27adae73
DevMarkLights <90109613+DevMarkLights@users.noreply.github.com>
2022-09-14 23:35:09 -0400

    54.34 into the tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'app/sign_in/sign_in_page.dart';
+
+//
 void main() {
-  runApp(MyApp());
-}
+  // this is the entry point of the app
+  runApp(MyApp()); // tells it to run your application
+} // every flutter app requires a main method
 
 class MyApp extends StatelessWidget {
+  // this is where the code for the app goes
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "time Tracker",
+      theme: ThemeData(
+        primarySwatch: Colors.indigo, // declaring the primary color
+      ),
+      home: SignInPage(),
+    );
   }
 }
